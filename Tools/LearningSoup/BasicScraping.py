01e988ea884d2d08bff15255666116da660e98ae
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-09-18 19:56:31 +0200

    Initial commit

---
+++ Tools/LearningSoup/BasicScraping.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.scrapethissite.com/pages/forms/"
+
+headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+
+requests.packages.urllib3.util.connection.HAS_IPV6 = False
+req = requests.get(url, headers=headers)
+# print(req)
+
+soup = BeautifulSoup(req.text, features='html.parser')
+# print(soup.prettify())
+
+p = soup.find_all('td', class_ = "name")
+# print(p)
+
+teams = []
+for t in p:
+    teams.append(t.text.strip())
+
+print(teams)
+# for t in teams:
+#     print(t)
+
+
