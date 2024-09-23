48360c506b41cd98d8c57e74fc79ebf4c92835bc
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-09-23 15:02:32 +0200

    worked a bit more on the smite scraper, not done yet

--- Tools/SmiteScraper.py
+++ Tools/SmiteScraper.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
-
 # print(requests.certs.where())
 
 url = "https://webcms.hirezstudios.com/smite2/api/posts/?filters[slug][$eq]=alpha-weekend-1-playtest-notes&lng=en-US&populate=*null"
-# url = "https://www.smite2.com/news/alpha-weekend-1-playtest-notes/"
 
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
@@ -12,8 +10,6 @@ requests.packages.urllib3.util.connection.HAS_IPV6 = False
 page = requests.get(url, headers=headers)
 # , verify=False
 # print(page.text)
-# test = page.json()
-# print(test)
 
 itemsDivIndex = page.text.find("<div>Items</div>")
 DefaultItemsStrongIndex = page.text.find("<strong>Default Items every god starts with every game</strong>")
@@ -32,26 +28,47 @@ unordered_lists = soup.find_all("ul", recursive=False)
 category = [categories.find_all("li", recursive=False) for categories in unordered_lists]
 # print(category_items)
 
+
 index = -1
 for items in category:
-    # if(index >= 1): 
-    for item in items:
-        # items_props = [e.text for e in item.contents]
-        # item_title = [item.contents[0]]
-        item_props = item.contents[1].find_all("li")
-        final_props = [item.contents[0], f"Tier: {index}"] + [prop.text for prop in item_props]
-        print(final_props)
+    if(index >= 1): 
+        for item in items:
+            # items_props = [e.text for e in item.contents]
+            # item_title = [item.contents[0]]
+            item_props = item.contents[1].find_all("li")
+            final_props1 = [item.contents[0], f"Tier: {index}"] + [prop.text for prop in item_props]
+            final_props2 = [prop.text for prop in item_props]
+            print(final_props1, final_props2)
+            
+            itemObj = {
+                "Name" : item.contents[0],
+                "Tier" : index,
+                "Gold" : (item_props[0].text)[:-1],
+                # for k, v in final_props:
+                # return 
+            }
+            print(itemObj)
+            
+            if(index >= 1): break
     index += 1
-        # break
-    # break
+    if(index >= 2): break
 
-    # class finished_item:
-    #     Name = item_title
-    #     Properties = item_props
-    # print(finished_item.Name, finished_item.Properties)
+# class finished_item:
+#     Name = item_title
+#     Properties = item_props
+# print(finished_item.Name, finished_item.Properties)
 
+# class Item(object):
+#     pass
 
-# print(items[0].find("li"))
+# itemObj = Item()
+# itemObj.Name = item.contents[0]
+# itemObj.Tier = index
+# itemObj.Gold = (item_props[0].text)[:-1]
+# print(itemObj)
 
-# items = [ li_item for li_item in ul_category ]
-# print(items[13])    
+class a() : pass
+obj = a()
+obj.a = 3
+setattr(obj, "Name", 2)
+print(vars(obj))
