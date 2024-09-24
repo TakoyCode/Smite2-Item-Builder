352b5a55db7f05900cd94c1a8104f100c797e1b4
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-09-24 15:23:33 +0200

    worked a tiny bit on SmiteScraper

--- Tools/SmiteScraper.py
+++ Tools/SmiteScraper.py
@@ -15,7 +15,6 @@ itemsDivIndex = page.text.find("<div>Items</div>")
 DefaultItemsStrongIndex = page.text.find("<strong>Default Items every god starts with every game</strong>")
 IssuesDivIndex = page.text.find("<div>Issues and Roadmap</div>")
 
-
 slicedPage = page.text[(DefaultItemsStrongIndex - 3):(IssuesDivIndex - 38)]
 
 soup = BeautifulSoup(slicedPage, features="html.parser")
@@ -28,30 +27,49 @@ unordered_lists = soup.find_all("ul", recursive=False)
 category = [categories.find_all("li", recursive=False) for categories in unordered_lists]
 # print(category_items)
 
+class a() : pass
+obj = a()
+obj.a = 3
+setattr(obj, "Name", 2)
+# print(vars(obj))
 
-index = -1
-for items in category:
-    if(index >= 1): 
-        for item in items:
-            # items_props = [e.text for e in item.contents]
-            # item_title = [item.contents[0]]
-            item_props = item.contents[1].find_all("li")
-            final_props1 = [item.contents[0], f"Tier: {index}"] + [prop.text for prop in item_props]
-            final_props2 = [prop.text for prop in item_props]
-            print(final_props1, final_props2)
-            
-            itemObj = {
-                "Name" : item.contents[0],
-                "Tier" : index,
-                "Gold" : (item_props[0].text)[:-1],
-                # for k, v in final_props:
-                # return 
-            }
-            print(itemObj)
-            
-            if(index >= 1): break
-    index += 1
-    if(index >= 2): break
+
+def GetItems():
+    index = -1
+    itemProps = []
+    for items in category:
+        if(index >= 1): 
+            for item in items:
+                item_name = [item.contents[0]]
+                item_props = item.contents[1].find_all("li")
+                # final_props1 = [item.contents[0], f"Tier: {index}"] + [prop.text for prop in item_props]
+                final_props2 = [prop.text for prop in item_props]
+
+                # Dra ut som metode+
+                if(":" in final_props2[1]):
+                    test = final_props2[1].split(": ", 1)
+                    print(test)
+                    continue
+                test = final_props2[1].split()
+                if(len(test) >= 3):
+                   test = ["".join(test[ :-1])] + test[-1:]
+
+                print(test)
+                # print((final_props2[1]).split())
+                
+                # final_props2[1]
+        index += 1
+    # return itemProps 
+GetItems()
+
+# print(itemObj)
+# itemObj = {
+#             "Name" : item.contents[0],
+#             "Tier" : index,
+#             "Gold" : (item_props[0].text)[:-1],
+#             # for k, v in final_props:
+#             # return 
+#         }
 
 # class finished_item:
 #     Name = item_title
@@ -67,8 +85,3 @@ for items in category:
 # itemObj.Gold = (item_props[0].text)[:-1]
 # print(itemObj)
 
-class a() : pass
-obj = a()
-obj.a = 3
-setattr(obj, "Name", 2)
-print(vars(obj))
