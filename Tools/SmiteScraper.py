0d29acdf8d715165b2b83f4338cb4489c0b718cb
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-09-24 21:28:53 +0200

    Made it so item props now are correctly formatted

--- Tools/SmiteScraper.py
+++ Tools/SmiteScraper.py
@@ -1,15 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+# This is for finding where the security certificate is located
 # print(requests.certs.where())
 
 url = "https://webcms.hirezstudios.com/smite2/api/posts/?filters[slug][$eq]=alpha-weekend-1-playtest-notes&lng=en-US&populate=*null"
 
 headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
+# makes it so the http request can't conenct via ipv6
 requests.packages.urllib3.util.connection.HAS_IPV6 = False
 page = requests.get(url, headers=headers)
-# , verify=False
-# print(page.text)
+# verify=False - use this if i ever have to disable the security certificate check in the http request
 
 itemsDivIndex = page.text.find("<div>Items</div>")
 DefaultItemsStrongIndex = page.text.find("<strong>Default Items every god starts with every game</strong>")
@@ -21,11 +22,41 @@ soup = BeautifulSoup(slicedPage, features="html.parser")
 # print(soup.prettify())
 
 unordered_lists = soup.find_all("ul", recursive=False)
-# print(ul_category[2])
 
-# li_items = ul_category[3].find_all("li", recursive=False)
 category = [categories.find_all("li", recursive=False) for categories in unordered_lists]
-# print(category_items)
+
+def FormatItemProp(prop):
+    if(": " in prop):
+        return prop.split(": ", 1)
+    
+    if("g" in prop[-1:]):
+        goldProp = ["Gold"] + [prop[:-1]]
+        return goldProp
+        
+    splitProps = prop.split()
+    if(len(splitProps) >= 3):
+        splitProps = (["".join(splitProps[ :-1])] + splitProps[-1:])
+
+    if("%" in splitProps[1][-1:]):
+        splitProps = [splitProps[0], splitProps[1][:-1]]
+
+    return splitProps
+
+def GetItems():
+    index = -1
+    for items in category:
+        if(index >= -1): 
+            for item in items:
+                itemProps = item.contents[1].find_all("li")
+                itemPropsText = [prop.text for prop in itemProps]
+
+                ItemWithFormatedProps = [["Name", item.contents[0]], ["Tier", index]]
+                for prop in itemPropsText:
+                    itemProp = FormatItemProp(prop)
+                    ItemWithFormatedProps.append(itemProp)
+                print(ItemWithFormatedProps)
+        index += 1
+GetItems()
 
 class a() : pass
 obj = a()
@@ -33,34 +64,6 @@ obj.a = 3
 setattr(obj, "Name", 2)
 # print(vars(obj))
 
-
-def GetItems():
-    index = -1
-    itemProps = []
-    for items in category:
-        if(index >= 1): 
-            for item in items:
-                item_name = [item.contents[0]]
-                item_props = item.contents[1].find_all("li")
-                # final_props1 = [item.contents[0], f"Tier: {index}"] + [prop.text for prop in item_props]
-                final_props2 = [prop.text for prop in item_props]
-
-                # Dra ut som metode+
-                if(":" in final_props2[1]):
-                    test = final_props2[1].split(": ", 1)
-                    print(test)
-                    continue
-                test = final_props2[1].split()
-                if(len(test) >= 3):
-                   test = ["".join(test[ :-1])] + test[-1:]
-
-                print(test)
-                # print((final_props2[1]).split())
-                
-                # final_props2[1]
-        index += 1
-    # return itemProps 
-GetItems()
 
 # print(itemObj)
 # itemObj = {
