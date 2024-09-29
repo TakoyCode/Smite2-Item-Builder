b28fd1a915187547af7ec2ffe8162bd275d7d672
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-09-29 19:31:03 +0200

    added bootstrap

--- Frontend/React/Smite2ItemBuilder/src/NavBar.jsx
+++ Frontend/React/Smite2ItemBuilder/src/NavBar.jsx
@@ -1,16 +1,26 @@
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 function NavBar() {
     return (
-        <div>
-            <img src=".\src\assets\Smite2Logo.png" alt="Smite2 Logo Icon" />
-            <div>Gods</div>
-            <div>Builds</div>
-            <div>Statistics</div>
-            <div>Item Builder</div>
-            <div>
-                <div>Sign In</div>
-                <img src=".\src\assets\Smite2-Logo_FINAL-Flat-OneColor-Gold1.png"
-                    alt="Smite2 Logo NoBackground" style={{ height: 100 }} />
-            </div>
+        <div className="container">
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a href="#" className="navbar-brand">
+                        <img src=".\src\assets\Smite2-Logo_FINAL-Flat-OneColor-Gold1.png"
+                            alt="Smite2 Logo NoBackground" width={110} height={47} />
+                    </a>
+                    <span className="navbar-text">Gods</span>
+                    <span className="navbar-text">Builds</span>
+                    <span className="navbar-text">Statistics</span>
+                    <span className="navbar-text">Item Builder</span>
+                    <a className="nav-link navbar-text" href="#">
+                        <span className="navbar-text">Sign In</span>
+                        <img src=".\src\assets\LoginIcon.png"
+                            alt="LoginIcon" height={28}
+                            className="d-inline-block align-text-top" />
+                    </a>
+                </div>
+            </nav >
         </div>
     );
 }
