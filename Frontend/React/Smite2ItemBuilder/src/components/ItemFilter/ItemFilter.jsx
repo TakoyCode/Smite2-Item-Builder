89373c8b97997a2ff02b90629b8089161d6d1bd2
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-10-21 19:50:15 +0200

    Succesfully refactored ItemFilter.jsx

--- Frontend/React/Smite2ItemBuilder/src/components/ItemFilter/ItemFilter.jsx
+++ Frontend/React/Smite2ItemBuilder/src/components/ItemFilter/ItemFilter.jsx
@@ -3,124 +3,63 @@ import './ItemFilter.css'
 export default function ItemFilter({ changeFilter }) {
     const filterCategories = [
         [
-            { imgName: "S2_Stat_All.png", fieldName: "Clear" },
+            { imgFileName: "S2_Stat_All.png", fieldName: "Clear" },
         ],
         [
-            { imgName: "S2_Stat_Strength.png", fieldName: "Strength" },
-            { imgName: "S2_Stat_Intelligence.png", fieldName: "Intelligence" },
+            { imgFileName: "S2_Stat_Strength.png", fieldName: "Strength" },
+            { imgFileName: "S2_Stat_Intelligence.png", fieldName: "Intelligence" },
         ],
         [
-            { imgName: "S2_Stat_AttackSpeed.png", fieldName: "Attack Speed" },
-            { imgName: "S2_Stat_Lifesteal.png", fieldName: "Lifesteal" },
-            { imgName: "S2_Stat_CriticalChance.png", fieldName: "Critical Chance" },
-            { imgName: "S2_Stat_Penetration.png", fieldName: "Penetration" },
+            { imgFileName: "S2_Stat_AttackSpeed.png", fieldName: "Attack Speed" },
+            { imgFileName: "S2_Stat_Lifesteal.png", fieldName: "Lifesteal" },
+            { imgFileName: "S2_Stat_CriticalChance.png", fieldName: "Critical Chance" },
+            { imgFileName: "S2_Stat_MagicalPen.png", fieldName: "Penetration" },
         ],
         [
-            { imgName: "S2_Stat_PhysicalProtection.png", fieldName: "Physical Protection" },
-            { imgName: "S2_Stat_MagicalProtection.png", fieldName: "Magical Protection" },
-            { imgName: "S2_Stat_MaxHealth.png", fieldName: "Max Health" },
-            { imgName: "S2_Stat_HealthRegen.png", fieldName: "Health Regen" },
+            { imgFileName: "S2_Stat_PhysicalProtection.png", fieldName: "Physical Protection" },
+            { imgFileName: "S2_Stat_MagicalProtection.png", fieldName: "Magical Protection" },
+            { imgFileName: "S2_Stat_MaxHealth.png", fieldName: "Max Health" },
+            { imgFileName: "S2_Stat_HealthRegen.png", fieldName: "Health Regen" },
         ],
         [
-            { imgName: "S2_Stat_MaxMana.png", fieldName: "Max Mana" },
-            { imgName: "S2_Stat_ManaRegen.png", fieldName: "Mana Regen" },
-            { imgName: "S2_Stat_HealingReduction.png", fieldName: "Healing Reduction" },
-            { imgName: "S2_Stat_CooldownRate.png", fieldName: "Cooldown Rate" },
+            { imgFileName: "S2_Stat_MaxMana.png", fieldName: "Max Mana" },
+            { imgFileName: "S2_Stat_ManaRegen.png", fieldName: "Mana Regen" },
+            { imgFileName: "S2_Stat_HealingReduction.png", fieldName: "Healing Reduction" },
+            { imgFileName: "S2_Stat_CooldownRate.png", fieldName: "Cooldown Rate" },
         ],
         [
-            { imgName: "S2_Stat_Active.png", fieldName: "Active" },
-            { imgName: "S2_Stat_Passive.png", fieldName: "Passive" },
+            { imgFileName: "S2_Stat_Active.png", fieldName: "Active" },
+            { imgFileName: "S2_Stat_Passive.png", fieldName: "Passive" },
         ],
     ]
 
+    function MapFilterCategory(filterCategory) {
+        return filterCategory.map((filterField, i) => {
+            return (
+                <div key={i} onClick={(e) => changeFilter(filterField.fieldName)}>
+                    <img src={`./src/assets/StatIcons/${filterField.imgFileName}`} alt={filterField.fieldName + " Stat Image"} />
+                    {filterField.fieldName}
+                </div>
+            );
+        })
+    }
+
     return (
-
-
         <div className="Item-Filter" style={{ width: 210 }}>
-            {/* {
-                filterCategories.map((filterField, i) => {
+            {
+                filterCategories.map((filterCategory, i) => {
                     return (
-                        <div onClick={(e) => changeFilter(filterField.fieldName)}>
-                            <img src=".\src\assets\StatIcons\S2_Stat_All.png" alt={filterField.fieldName} />
-                            {filterField.fieldName}
-                        </div>
+                        <>
+                            {
+                                i != 0 ? <hr /> : ""
+                            }
+                            {
+                                MapFilterCategory(filterCategory)
+                            }
+                        </>
                     );
-
                 })
-            } */}
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_All.png" alt="" />
-                Clear
-            </div>
-            <hr />
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_Strength.png" alt="" />
-                Strength
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_Intelligence.png" alt="" />
-                Intelligence
-            </div>
-            <hr />
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_AttackSpeed.png" alt="" />
-                Attack Speed
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_Lifesteal.png" alt="" />
-                Lifesteal
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_CriticalChance.png" alt="" />
-                Critical Chance
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_MagicalPen.png" alt="" />
-                Penetration
-            </div>
-            <hr />
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_PhysicalProtection.png" alt="" />
-                Physical Protection
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_MagicalProtection.png" alt="" />
-                Magical Protection
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_MaxHealth.png" alt="" />
-                Max Health
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_HealthRegen.png" alt="" />
-                Health Regen
-            </div>
-            <hr />
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_MaxMana.png" alt="" />
-                Max Mana
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_ManaRegen.png" alt="" />
-                Mana Regen
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_HealingReduction.png" alt="" />
-                Healing Reduction
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_CooldownRate.png" alt="" />
-                Cooldown Rate
-            </div>
-            <hr />
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_Active.png" alt="" />
-                Active
-            </div>
-            <div onClick={(e) => changeFilter(e.target.innerText)}>
-                <img src=".\src\assets\StatIcons\S2_Stat_Passive.png" alt="" />
-                Passive
-            </div>
-        </div>
+            }
+        </div >
     )
 }
