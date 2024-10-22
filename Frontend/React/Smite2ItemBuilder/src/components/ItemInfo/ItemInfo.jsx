27930d9d882282fce104faf4fe666d99b4a0ee8a
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-10-22 20:52:12 +0200

    small css changes

--- Frontend/React/Smite2ItemBuilder/src/components/ItemInfo/ItemInfo.jsx
+++ Frontend/React/Smite2ItemBuilder/src/components/ItemInfo/ItemInfo.jsx
@@ -16,36 +16,38 @@ export default function ItemInfo({ item }) {
             <div className="Item-Tree text-center rounded mt-4">Item Tree Showcase</div>
             <div className="Item-Info rounded mt-3 p-4">
                 <h3 className="fw-bold" style={{ color: "rgb(186, 190, 195)" }}>{item.Name}</h3>
-                {
-                    itemEntry.map(([k, v], i) => {
-                        if (!v) return;
-                        if (noShowProps.includes(k)) return;
-                        return (
-                            <div className="mt-1" key={i}>
-                                <img src={`./src/assets/StatIcons/S2_Stat_${k.replaceAll(" ", "")}.png`} alt="" className="me-1" style={{ height: 25 }} />
-                                <span style={{ color: "rgb(186, 190, 195)" }}
-                                >{v}{showPropsInPercent.includes(k) ? "%" : ""} {k}</span>
-                            </div>
-                        )
-                    })
-                }
-                {
-                    item.Passive != null || item.Active != null ? <div><hr /></div> : ""
-                }
-                {
-                    itemEntry.map(([k, v], i) => {
-                        if (!v) return;
-                        if (k === "Passive" || k === "Active") {
+                <div>
+                    {
+                        itemEntry.map(([k, v], i) => {
+                            if (!v) return;
+                            if (noShowProps.includes(k)) return;
                             return (
-                                <div key={i}>
+                                <div className="mt-1" key={i}>
                                     <img src={`./src/assets/StatIcons/S2_Stat_${k.replaceAll(" ", "")}.png`} alt="" className="me-1" style={{ height: 25 }} />
-                                    <span style={{ color: "rgb(186, 190, 195)" }}>{k}: {v}</span>
+                                    <span style={{ color: "rgb(186, 190, 195)" }}
+                                    >{v}{showPropsInPercent.includes(k) ? "%" : ""} {k}</span>
                                 </div>
                             )
-                        }
-                    })
-                }
+                        })
+                    }
+                    {
+                        item.Passive != null || item.Active != null ? <div><hr /></div> : ""
+                    }
+                    {
+                        itemEntry.map(([k, v], i) => {
+                            if (!v) return;
+                            if (k === "Passive" || k === "Active") {
+                                return (
+                                    <div key={i}>
+                                        <img src={`./src/assets/StatIcons/S2_Stat_${k.replaceAll(" ", "")}.png`} alt="" className="me-1" style={{ height: 25 }} />
+                                        <span style={{ color: "rgb(186, 190, 195)" }}>{k}: {v}</span>
+                                    </div>
+                                )
+                            }
+                        })
+                    }
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
