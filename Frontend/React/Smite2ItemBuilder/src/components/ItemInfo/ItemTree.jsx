fe78a35f96f5a87f2d441800ef8b2d953d8e284b
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-11-07 22:29:49 +0100

    more work on ItemTree

--- Frontend/React/Smite2ItemBuilder/src/components/ItemInfo/ItemTree.jsx
+++ Frontend/React/Smite2ItemBuilder/src/components/ItemInfo/ItemTree.jsx
@@ -4,9 +4,8 @@ import ItemWOnclick from "../Item/ItemWOnclick";
 
 export default function ItemTree({ item, itemRecipes }) {
     const [items, setItems] = useState(useLoaderData());
-    const [itemRecipe, setItemRecipe] = useState(null);
     const [recipeItems, setRecipeItems] = useState([]);
-    // const [itemTree, setSetItemTree] = useState([]);
+    const [itemTree, setItemTree] = useState([]);
 
     // ["ItemMain", ["Component1", ["Component1.1"]], ["Component2"]]
 
@@ -16,130 +15,106 @@ export default function ItemTree({ item, itemRecipes }) {
     // }
 
     useEffect(() => {
-        console.log(buildTree(item.Id));
-    }, [])
+        const builtItemTree = buildItemTree(item.Id);
+        setItemTree(builtItemTree);
+    }, [item])
+
+    useEffect(() => console.log(drawItemTree(itemTree)), [itemTree])
 
     // Helper to find an item by ID
-    const findItemById = (id) => items.find(item => item.id === id);
+    const findItemById = (id) => items.find(item => item.Id === id);
 
     // Helper to find the recipe by MainItemId
     const findRecipeByMainItemId = (id) => itemRecipes.find(recipe => recipe.MainItemId === id);
 
     // Recursive function to build the tree
-    function buildTree(mainItemId) {
+    function buildItemTree(mainItemId) {
         const itemRecipe = findRecipeByMainItemId(mainItemId);
 
+        // Base case
         if (!itemRecipe) {
-            // return { MainItemId: mainItemId, components: [] };
-            return [mainItemId];
+            return { MainItemId: mainItemId, components: [] };
         }
 
-        const tree = [itemRecipe.MainItemId, []]
+        // Recursive case
+        const itemTree = {
+            MainItemId: itemRecipe.MainItemId,
+            components: []
+        };
 
-        // const tree = {
-        //     MainItemId: itemRecipe.MainItemId,
-        //     components: []
-        // };
+        itemRecipe.Component1Id && itemRecipe.Component2Id ?
+            itemTree.components.push(buildItemTree(itemRecipe.Component1Id), buildItemTree(itemRecipe.Component2Id)) :
+            itemTree.components.push(buildItemTree(itemRecipe.Component1Id));
 
-        if (itemRecipe.Component1Id) {
-            // tree.components.push(buildTree(itemRecipe.Component1Id));
-            (tree[1]).push(buildTree(itemRecipe.Component1Id));
-        }
-        if (itemRecipe.Component2Id) {
-            // tree.components.push(buildTree(itemRecipe.Component2Id));
-            (tree[1]).push(buildTree(itemRecipe.Component2Id));
-        }
-
-        return tree;
+        // return finished item tree
+        return itemTree;
     }
 
+    function drawItemTree(tree) {
+        const mainItemId = tree.MainItemId;
+        const mainItem = findItemById(mainItemId);
 
-    // function getItemTree(itemId) {
-    //     const itemTree = [];
-    //     // Find recipe that creates item
-    //     const MainItemRecipe = itemRecipes?.find((recipe) => recipe.MainItemId === itemId);
+        // Base case
+        if (!tree.components) {
+            return <ItemWOnclick item={mainItem} onClickFunction={null} />;
+        }
 
-    //     if (MainItemRecipe) {
-    //         console.log(MainItemRecipe)
+        // Recursive case
+        const itemTree = [<ItemWOnclick item={mainItem} onClickFunction={null} />];
 
-    //         for (const [key, componentId] of Object.entries(MainItemRecipe)) {
-    //             if (key === "Id" || key === "MainItemId" || componentId === null) continue;
+        // let component1 = "";
+        // let component2 = "";
+        if (tree.components[0]) {
+            // component1 = drawItemTree(tree.components[0]);
+            itemTree.push(drawItemTree(tree.components[0]));
+        }
+        if (tree.components[1]) {
+            // component2 = drawItemTree(tree.components[1]);
+            itemTree.push(drawItemTree(tree.components[1]));
+        }
 
-    //             const foundComponentRecipe = itemRecipes?.find((recipe) => recipe.MainItemId === componentId);
-    //             if (foundComponentRecipe) {
-    //                 console.log(foundComponentRecipe)
+        // const itemTree = [
+        //     <div className="d-flex justify-content-evenly">
+        //         {component1}
+        //         {component2}
+        //     </div>
+        // ]
 
-    //                 for (const [key, componentId] of Object.entries(foundComponentRecipe)) {
-    //                     if (key === "Id" || key === "MainItemId" || componentId === null) continue;
-
-    //                     const foundComponentComponentRecipe = itemRecipes?.find((recipe) => recipe.MainItemId === componentId);
-    //                     if (foundComponentComponentRecipe) {
-    //                         console.log(foundComponentComponentRecipe)
-    //                     }
-    //                 }
-    //             }
-
-    //         }
-    //     }
-    // }
-
-    // useEffect(() => {
-    //     getItemRecipe();
-    // }, [item])
-
-    // useEffect(() => {
-    //     getRecipeItems();
-    // }, [itemRecipe])
-
-    // function getRecipeItems() {
-    //     if (itemRecipe) {
-    //         let recipeItemsTemp = [];
-    //         for (let [key, value] of Object.entries(itemRecipe)) {
-    //             if (key === "Id") continue;
-    //             const recipeItem = items.find((item) => item.Id === value);
-    //             recipeItemsTemp.push(recipeItem ? recipeItem : null)
-    //         }
-    //         setRecipeItems(recipeItemsTemp);
-    //     }
-    //     else setRecipeItems([]);
-    // }
-
-    // function getItemRecipe() {
-    //     const itemRecipeTemp = itemRecipes?.find((recipe) => recipe.MainItemId === item.Id)
-    //     if (itemRecipeTemp) setItemRecipe(itemRecipeTemp);
-    //     else setItemRecipe(null);
-    // }
+        // return finished item tree
+        return itemTree;
+    }
 
     return (
         <div className="Item-Tree text-center rounded mt-4" style={{ height: "45%", background: " rgb(26, 40, 57)" }}>
             <div className="d-flex flex-column align-items-center mt-3">
+                {/* <div className="d-flex justify-content-evenly">
+                    <ItemWOnclick item={item} onClickFunction={null} />
+                </div>
                 <div className="d-flex justify-content-evenly">
                     {
-                        recipeItems?.map((item, index) => {
-                            if (item?.Tier === 3) {
-                                return <ItemWOnclick key={index} item={item} onClickFunction={null} />
-                            }
+                        itemTree.components?.map((componentTree, index) => {
+                            const itemId = componentTree.MainItemId;
+                            const item = findItemById(itemId);
+                            return <ItemWOnclick key={index} item={item} onClickFunction={null} />
                         })
                     }
                 </div>
                 <div className="d-flex justify-content-evenly">
                     {
-                        recipeItems?.map((item, index) => {
-                            if (item?.Tier === 2) {
-                                return <ItemWOnclick key={index} item={item} onClickFunction={null} />
-                            }
-                        })
+                        itemTree.components?.map((componentTree) => componentTree.components)
+                        // ?.map((componentTree, index) => {
+                        //     console.log(componentTree)
+                        //     const itemId = componentTree.MainItemId;
+                        //     console.log(itemId)
+                        //     const item = findItemById(itemId);
+                        //     return <ItemWOnclick key={index} item={item} onClickFunction={null} />
+                        // })
                     }
-                </div>
-                <div className="d-flex justify-content-evenly">
-                    {
-                        recipeItems?.map((item, index) => {
-                            if (item?.Tier === 1) {
-                                return <ItemWOnclick key={index} item={item} onClickFunction={null} />
-                            }
-                        })
-                    }
-                </div>
+                </div> */}
+                {
+                    drawItemTree(itemTree)
+
+                }
             </div>
 
         </div>
